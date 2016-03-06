965772bc5008105e7aa09749071765fdd83e470e
Graham Ollis <plicease@cpan.org>
2016-03-06 16:01:14 -0500

    initial implementation

--- lib/Alien/Builder/Interpolator/TextTemplate.pm
+++ lib/Alien/Builder/Interpolator/TextTemplate.pm
@@ -2,8 +2,57 @@ package Alien::Builder::Interpolator::TextTemplate;
 
 use strict;
 use warnings;
+use Text::Template;
+use base qw( Alien::Builder::Interpolator );
 
 # ABSTRACT: Alien::Builder interpolator using Text::Template
 # VERSION
+
+=head1 METHODS
+
+=head2 interpolate
+
+ my $string = $itr->interpolate($template);
+
+=cut
+
+sub interpolate
+{
+  my($self, $string) = @_;
+  
+  return undef unless defined $string;
+  
+  my $template = Text::Template->new(
+    TYPE   => 'STRING',
+    SOURCE => $string,
+  );
+  
+  my $helper = Alien::Builder::Interpolator::TextTemplate::Helpers->new($self);
+  
+  $template->fill_in(
+    PACKAGE => 'Foo',
+    HASH => {
+      %{ $self->vars },
+      helper => \$helper,
+    },
+  );
+}
+
+package Alien::Builder::Interpolator::TextTemplate::Helpers;
+
+use strict;
+use warnings;
+use Object::Method ();
+
+sub new
+{
+  my($class, $interpolator) = @_;
+  my $self = bless { }, $class;
+  foreach my $method (keys %{ $interpolator->helpers })
+  {
+    Object::Method::method($self, $method => sub { $interpolator->execute_helper($method) });
+  }
+  $self;
+}
 
 1;
